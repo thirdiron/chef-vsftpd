2f1a738a19f5b14f319bffe7309df3ba3d8a53e4
John Muth <johnmuthuk@gmail.com>
2022-07-06 14:00:05 +0100

    Update _define_service.rb

--- recipes/_define_service.rb
+++ recipes/_define_service.rb
@@ -2,14 +2,14 @@
 
 if node['vsftpd']['enabled']
   service 'vsftpd' do
-    provider Chef::Provider::Service::Upstart if node['platform'] == 'ubuntu' &&
+    provider Chef::Provider::Service::Systemd if node['platform'] == 'ubuntu' &&
                                                  node['platform_version'].to_f >= 14.04
     action [:enable, :start]
     supports restart: true
   end
 else
   service 'vsftpd' do
-    provider Chef::Provider::Service::Upstart if node['platform'] == 'ubuntu' &&
+    provider Chef::Provider::Service::Systemd if node['platform'] == 'ubuntu' &&
                                                  node['platform_version'].to_f >= 14.04
     action [:disable, :stop]
     supports restart: true
